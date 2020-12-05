917744f57d2b9d61edd2c6044d2cdc596f45fa2d
retryme <retryme@hotmail.com>
2020-12-05 13:57:40 +0300

    comm

--- csvtojason.py
+++ csvtojason.py
@@ -13,6 +13,7 @@ for root, dirs, files in os.walk('/home/root1/Desktop/samples'):
         dir_er = '/home/root1/Desktop/samples' + '/' + files[i]
         #print(dir_er)
         direc_list.append(dir_er)
+#deneme
 
 leng_of_direclist = len(direc_list)
 
